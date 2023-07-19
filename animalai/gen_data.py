365faf6f567fcffcfff33f952c9e13b9f0dd153f
mihoKuroki <131640567+mihoKuroki@users.noreply.github.com>
2023-07-20 01:03:46 +0900

    1

--- animalai/gen_data.py
+++ animalai/gen_data.py
@@ -1,1 +1,33 @@
 #画像データをNumPy配列に変換する
+from PIL import Image
+import os, glob
+import numpy as np
+from sklearn import model_selection
+
+classes = ["monkey", "boar", "crow"]
+num_classes = len(classes)
+image_size = 50
+
+
+#画像の読み込み
+X = []  #画像データ
+Y = []  #ラベルデータ
+for index, classlabel in enumerate(classes):
+    photos_dir = "./" + classlabel
+    files = glob.glob(photos_dir + "/*.jpg")
+    for i, file in enumerate(files):
+        if i >= 30: break
+        image = Image.open(file)  #画像を開く
+        image = image.convert("RGB")  #色空間をRGBに
+        image = image.resize((image_size, image_size))  #画像サイズを50*50に変換
+        data = np.asarray(image)  #NumPy配列に変換
+        X.append(data)  #asarrayでNumPy配列に変換
+        Y.append(index)  #ラベルを追加
+        
+X = np.array(X)
+Y = np.array(Y)
+
+#学習データとテストデータを分ける
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, Y)
+xy = (X_train, X_test, y_train, y_test)
+np.save("./animal.npy", xy)  #NumPy配列をバイナリファイルに保存
